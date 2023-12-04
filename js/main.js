02d5c5e246cc905890703060224669edf0722bb9
ZHANG Xin <zhang284u@etu.univ-lorraine.fr>
2023-12-04 14:44:19 +0100

    changement

--- js/main.js
+++ js/main.js
@@ -10,6 +10,23 @@ let accueil = document.getElementById("container-accueil");
 let learn = document.getElementById("container-learning");
 let train = document.getElementById("training-container");
 
+//READ Dict
+let dict = [];
+fetch("resources/dict/dict.json")
+    .then(response => response.json())
+    .then(json => {
+        dict = json["dict"];
+        shuffleList(dict);
+    })
+    .catch(err => console.log(err));
+
+function shuffleList(list) {
+    for (let i = list.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [list[i], list[j]] = [list[j], list[i]];
+    }
+}
+
 function checkSpeechAvailable(){
     if('speechSynthesis' in window) {
         speechMSG.innerHTML = "Votre navigateur supporte la synthèse de la parole"
@@ -52,17 +69,42 @@ function speak(text) {
 
 window.onload = checkSpeechAvailable
 
+let cards = [];
+
 document.getElementById("startButton").onclick = function () {
     accueil.setAttribute("hidden", "hidden");
-    learn.removeAttribute("hidden");
+    train.removeAttribute("hidden");
+    loadCards();
 }
 
 document.getElementById("training-button").onclick = function () {
-    learn.setAttribute("hidden", "hidden");
-    train.removeAttribute("hidden");
+    train.setAttribute("hidden", "hidden");
+    learn.removeAttribute("hidden");
 }
 
 document.getElementById("voiceTest").onclick = function () {
     speak("Bonjour");
+}
+
+function loadCards(lang = "English"){
+    let nbCards = 9;
+    cards = dict.slice(0, nbCards);
+    for (let i = 0; i < nbCards; i++) {
+        let card = createCard(cards[i]["language"][lang], cards[i]["path"]);
+        document.getElementById("train-cards").appendChild(card);
+    }
+}
+
+function createCard(title, imgPath){
+    let card = document.createElement('div');
+    card.classList.add("card");
+    let img = document.createElement('img');
+    img.src = imgPath;
+    let cardTitle = document.createElement('h2');
+    cardTitle.innerHTML = title;
+    cardTitle.classList.add("card-title");
+    card.appendChild(img);
+    card.appendChild(cardTitle);
+    return card;
 }
 
